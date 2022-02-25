aa3acc951effdd3dcdcbd7236996d3b17cc7251a
Santosl2c <mfilype2019@gmail.com>
2022-02-25 11:01:51 -0300

    feat: user avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 import { CreateUserService } from "../services/CreateUserService";
+import { UpdateUserAvatarService } from "../services/UpdateUserAvatarService";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.get("/", (request, response) => {
   try {
@@ -22,5 +27,27 @@ usersRouter.post("/", async (request, response) => {
     return response.status(400).json({ error: err?.message });
   }
 });
+
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      if (!request.file) throw new Error("Oops, você se esquece da imagem.");
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user?.password;
+
+      return response.json(user);
+    } catch (err: Error | any) {}
+  },
+);
 
 export default usersRouter;
